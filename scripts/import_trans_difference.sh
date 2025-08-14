1e9bae5e94cbeaf62d47eecdbf39ff8633edb7c1
Damiano Testa <adomani@gmail.com>
2025-08-14 07:16:22 +0200

    adjust docs

--- scripts/import_trans_difference.sh
+++ scripts/import_trans_difference.sh
@@ -133,7 +133,7 @@ printf '\n\n<details><summary>Import changes for all files</summary>\n\n%s\n\n</
       }
     }
     if ((all == "false") && (ghLimit/2 <= outputLength)) {
-      printf("There are %s files with changed transitive imports taking up over %s characters: this is too many to display!\nYou can run this locally by cloning <a href=\"https://github.com/adomani/PR_summary\">adomani/PR_summary</a> and then using `scripts/import_trans_difference.sh all` locally to see the whole output.", fileCount, outputLength)
+      printf("There are %s files with changed transitive imports taking up over %s characters: this is too many to display!\nYou can run this locally by cloning <a href=\"https://github.com/adomani/PR_summary\">adomani/PR_summary</a> and then using `scripts/import_trans_difference.sh -a` locally to see the whole output.", fileCount, outputLength)
     } else {
       for(x in reds) {
         sorted=""
